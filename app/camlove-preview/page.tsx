5198ce03406994771014ece8c0927fc74a5d564a
Patrik <125508378+pako999@users.noreply.github.com>
2026-08-13 18:51:26 +0200

    Add CamLove preview route with pricing

---
+++ app/camlove-preview/page.tsx
@@ -0,0 +1,11 @@
+import CamLoveRedesignPage from "../camlove-redesign/page";
+import { CamLovePricingAppend } from "@/components/CamLovePricingAppend";
+
+export default function CamLovePreviewPage() {
+  return (
+    <>
+      <CamLoveRedesignPage />
+      <CamLovePricingAppend />
+    </>
+  );
+}
